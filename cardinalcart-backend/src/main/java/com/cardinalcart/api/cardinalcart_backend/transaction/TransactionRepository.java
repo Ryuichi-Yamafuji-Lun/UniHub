dff85af8f366796e3aa10b26a6b5cd0398d710e1
Ryuichi Yamafuji Lun <92515437+Ryuichi-Yamafuji-Lun@users.noreply.github.com>
2025-04-07 16:51:53 -0700

    Create the repo for listing and transaction

---
+++ cardinalcart-backend/src/main/java/com/cardinalcart/api/cardinalcart_backend/transaction/TransactionRepository.java
@@ -0,0 +1,9 @@
+package com.cardinalcart.api.cardinalcart_backend.transaction;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long>{
+
+
+    
+}
