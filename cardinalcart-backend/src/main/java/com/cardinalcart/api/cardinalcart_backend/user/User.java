e23bcb24d53f39eb46a9384135e8c96b1a4e2cba
Ryuichi Yamafuji Lun <92515437+Ryuichi-Yamafuji-Lun@users.noreply.github.com>
2025-04-06 20:12:27 -0700

    create initial user

--- cardinalcart-backend/src/main/java/com/cardinalcart/api/cardinalcart_backend/user/User.java
+++ cardinalcart-backend/src/main/java/com/cardinalcart/api/cardinalcart_backend/user/User.java
@@ -1,5 +1,134 @@
 package com.cardinalcart.api.cardinalcart_backend.user;
 
+import com.cardinalcart.api.cardinalcart_backend.role.Role; 
+import com.cardinalcart.api.cardinalcart_backend.userstatus.UserStatus;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    // User private information 
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+
+    // User school email
+    private String schoolEmail;
+
+    // User account track
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    // Track User activity and permission
+    private Role role;
+    private UserStatus userStatus;
+
+    // For JPA
+    public User(){}
+
+    // Set User
+    public User(String firstName, String lastName, LocalDate dateOfBirth, String schoolEmail, LocalDateTime createdAt,
+            LocalDateTime updatedAt, Role role, UserStatus userStatus) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.schoolEmail = schoolEmail;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.role = role;
+        this.userStatus = userStatus;
+    }
+
+    // Getter & Setter
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getSchoolEmail() {
+        return schoolEmail;
+    }
+
+    public void setSchoolEmail(String schoolEmail) {
+        this.schoolEmail = schoolEmail;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public UserStatus getUserStatus() {
+        return userStatus;
+    }
+
+    public void setUserStatus(UserStatus userStatus) {
+        this.userStatus = userStatus;
+    }
+
+    // Return user information in string format
+    @Override
+    public String toString() {
+        return "User {" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", schoolEmail='" + schoolEmail + '\'' +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                ", role=" + role +
+                ", userStatus=" + userStatus +
+                '}';
+    }
 
 }
---
+++ cardinalcart-backend/src/main/java/com/cardinalcart/api/cardinalcart_backend/userstatus/UserStatus.java
@@ -0,0 +1,8 @@
+package com.cardinalcart.api.cardinalcart_backend.userstatus;
+
+public enum UserStatus {
+    ACTIVE,
+    BANNED,
+    SUSPENDED,
+    DELETED
+}
