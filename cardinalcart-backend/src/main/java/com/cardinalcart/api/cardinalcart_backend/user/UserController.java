80c1bb1b491f4facde3e8cd41ac65f8c96a8f52b
Ryuichi Yamafuji Lun <92515437+Ryuichi-Yamafuji-Lun@users.noreply.github.com>
2025-04-08 16:48:42 -0700

    Added the functions to update user status and role and to read user status and role

--- cardinalcart-backend/src/main/java/com/cardinalcart/api/cardinalcart_backend/user/UserController.java
+++ cardinalcart-backend/src/main/java/com/cardinalcart/api/cardinalcart_backend/user/UserController.java
@@ -7,10 +7,14 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cardinalcart.api.cardinalcart_backend.userstatusrole.Role;
+import com.cardinalcart.api.cardinalcart_backend.userstatusrole.UserStatus;
 import com.cardinalcart.api.cardinalcart_backend.userstatusrole.UserStatusRoleResponse;
 
 @RestController
@@ -53,5 +57,16 @@ public class UserController {
 
         return ResponseEntity.ok(userStatusRoleResponse);
     }
-    
+
+    @PutMapping("/{id}/role")
+    public ResponseEntity<User> updateUserRole(@PathVariable Long id, @RequestParam Role role) {
+        User user = userService.updateUserRole(id, role);
+        return ResponseEntity.ok(user);
+    }
+
+    @PutMapping("/{id}/status")
+    public ResponseEntity<User> changeUserStatus(@PathVariable Long id, @RequestParam UserStatus userStatus) {
+        User user = userService.updateUserStatus(id, userStatus);
+        return ResponseEntity.ok(user);
+    }
 }
