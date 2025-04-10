da2ae1db29812d0089dd7e232e391fb5bbcb4cc7
Ryuichi Yamafuji Lun <92515437+Ryuichi-Yamafuji-Lun@users.noreply.github.com>
2025-04-09 21:10:53 -0700

    invluded accountService

--- cardinalcart-backend/src/main/java/com/cardinalcart/api/cardinalcart_backend/listing/ListingController.java
+++ cardinalcart-backend/src/main/java/com/cardinalcart/api/cardinalcart_backend/listing/ListingController.java
@@ -3,14 +3,21 @@ package com.cardinalcart.api.cardinalcart_backend.listing;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cardinalcart.api.cardinalcart_backend.account.AccountService;
+
 @RestController
 @RequestMapping(path = "api/v1/listing")
 public class ListingController {
 
     private final ListingService listingService;
+    private final AccountService accountService;
 
-    public ListingController(ListingService listingService) {
+    public ListingController(ListingService listingService, AccountService accountService) {
         this.listingService = listingService;
+        this.accountService = accountService;
     }
+
+    
+  
 
 }
