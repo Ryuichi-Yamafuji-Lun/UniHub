41d0c35c13b81ef8d0f3738f84c2b74c8fceecb6
Ryuichi Yamafuji Lun <92515437+Ryuichi-Yamafuji-Lun@users.noreply.github.com>
2025-04-09 22:24:28 -0700

    create delete listing and edit postmapping

--- cardinalcart-backend/src/main/java/com/cardinalcart/api/cardinalcart_backend/listing/ListingController.java
+++ cardinalcart-backend/src/main/java/com/cardinalcart/api/cardinalcart_backend/listing/ListingController.java
@@ -1,6 +1,8 @@
 package com.cardinalcart.api.cardinalcart_backend.listing;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,10 +23,20 @@ public class ListingController {
         this.accountService = accountService;
     }
 
-    @PostMapping
+    @PostMapping("/{accountId}")
     public ResponseEntity<Listing> createListing(@RequestBody Listing listing, @PathVariable Long accountId) {
         Listing createdListing = listingService.createListing(listing, accountId);
         return ResponseEntity.status(201).body(createdListing);
+    }
+
+    @DeleteMapping("/{accountId}/{listingId}")
+    public ResponseEntity<Void> deleteListing(@PathVariable Long accountId, @PathVariable Long listingId) {
+        try {
+            listingService.deleteListing(listingId, accountId);
+            return ResponseEntity.noContent().build();
+        } catch (RuntimeException ex) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 
 }
