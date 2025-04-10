b73ab6bad9f40e8911760bda6d051c9b6cc3188f
Ryuichi Yamafuji Lun <92515437+Ryuichi-Yamafuji-Lun@users.noreply.github.com>
2025-04-09 19:14:54 -0700

    added validation to listing

--- cardinalcart-backend/src/main/java/com/cardinalcart/api/cardinalcart_backend/listing/ListingService.java
+++ cardinalcart-backend/src/main/java/com/cardinalcart/api/cardinalcart_backend/listing/ListingService.java
@@ -5,14 +5,18 @@ import java.time.LocalDateTime;
 import org.springframework.stereotype.Service;
 
 import com.cardinalcart.api.cardinalcart_backend.account.Account;
+import com.cardinalcart.api.cardinalcart_backend.account.AccountRepository;
 
 @Service
 public class ListingService {
 
     public final ListingRepository listingRepository;
+    public final AccountRepository accountRepository;
 
-    public ListingService(ListingRepository listingRepository) {
+
+    public ListingService(ListingRepository listingRepository, AccountRepository accountRepository) {
         this.listingRepository = listingRepository;
+        this.accountRepository = accountRepository;
     }
 
     // create listing
@@ -20,8 +24,20 @@ public class ListingService {
         // Do a double take on account and date posted
         listing.setAccount(account);
         listing.setDatePosted(LocalDateTime.now());
-        
+
         return listingRepository.save(listing);
     }
 
+    // // delete listing
+    // public void 
+
+    // check if account exists in the database
+    public boolean isValidAccount(Account account) {
+        return accountRepository.findById(account.getId()).isPresent();
+    }
+
+    // check if user is the owner of the listing
+    public boolean isOwnerOfListing(Listing listing, Account account) {
+        return listing.getAccount().equals(account);
+    }
 }
