16193085ac18b013778284f50d03bf1084892795
Ryuichi Yamafuji Lun <92515437+Ryuichi-Yamafuji-Lun@users.noreply.github.com>
2025-04-15 18:19:35 -0700

    initial commit for splitting the service for account

---
+++ cardinalcart-backend/src/main/java/com/cardinalcart/api/cardinalcart_backend/account/admin/AdminAccountService.java
@@ -0,0 +1,8 @@
+package com.cardinalcart.api.cardinalcart_backend.account.admin;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminAccountService {
+
+}
---
+++ cardinalcart-backend/src/main/java/com/cardinalcart/api/cardinalcart_backend/account/user/UserAccountService.java
@@ -0,0 +1,8 @@
+package com.cardinalcart.api.cardinalcart_backend.account.user;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserAccountService {
+
+}
