daa41e0e39f7b16a7542c643279d6a104f43d10c
Ryuichi Yamafuji Lun <92515437+Ryuichi-Yamafuji-Lun@users.noreply.github.com>
2025-04-16 12:42:48 -0700

    added listing repository to both owner and public service layer

--- cardinalcart-backend/src/main/java/com/cardinalcart/api/cardinalcart_backend/listing/owneraccess/OwnerListingService.java
+++ cardinalcart-backend/src/main/java/com/cardinalcart/api/cardinalcart_backend/listing/owneraccess/OwnerListingService.java
@@ -2,7 +2,16 @@ package com.cardinalcart.api.cardinalcart_backend.listing.owneraccess;
 
 import org.springframework.stereotype.Service;
 
+import com.cardinalcart.api.cardinalcart_backend.listing.ListingRepository;
+
 @Service
 public class OwnerListingService {
 
+    public final ListingRepository listingRepository;
+
+    public OwnerListingService(ListingRepository listingRepository) {
+        this.listingRepository = listingRepository;
+    }
+
+    
 }
--- cardinalcart-backend/src/main/java/com/cardinalcart/api/cardinalcart_backend/listing/publicaccess/PublicListingService.java
+++ cardinalcart-backend/src/main/java/com/cardinalcart/api/cardinalcart_backend/listing/publicaccess/PublicListingService.java
@@ -2,7 +2,16 @@ package com.cardinalcart.api.cardinalcart_backend.listing.publicaccess;
 
 import org.springframework.stereotype.Service;
 
+import com.cardinalcart.api.cardinalcart_backend.listing.ListingRepository;
+
 @Service
 public class PublicListingService {
 
+    public final ListingRepository listingRepository;
+
+    public PublicListingService(ListingRepository listingRepository) {
+        this.listingRepository = listingRepository;
+    }
+
+    
 }
