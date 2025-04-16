21192336ef50072a848d3f957c21048adc7b0b87
Ryuichi Yamafuji Lun <92515437+Ryuichi-Yamafuji-Lun@users.noreply.github.com>
2025-04-16 11:56:24 -0700

    created user and public account controllers

---
+++ cardinalcart-backend/src/main/java/com/cardinalcart/api/cardinalcart_backend/account/user/UserAccountController.java
@@ -0,0 +1,41 @@
+package com.cardinalcart.api.cardinalcart_backend.account.user;
+
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cardinalcart.api.cardinalcart_backend.account.Account;
+
+@RestController
+@RequestMapping(path = "api/v2/user/account")
+public class UserAccountController {
+
+    private final UserAccountService accountService;
+
+    public UserAccountController(UserAccountService accountService) {
+        this.accountService = accountService;
+    }
+    
+   @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAccount(@PathVariable Long id) {  
+        try {
+            accountService.deleteAccount(id);
+            return ResponseEntity.noContent().build();
+        } catch (RuntimeException ex) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Account> getAccountById(@PathVariable Long id) {
+        Optional<Account> account = accountService.findAccountById(id);
+        return account.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+   
+}
