7fd16136bc99e6849ac2cf174c83c78920bd67d6
Ryuichi Yamafuji Lun <92515437+Ryuichi-Yamafuji-Lun@users.noreply.github.com>
2025-05-17 10:32:12 -1000

    initial commit for public sublease controller

--- unihub-backend/src/main/java/com/unihub/api/unihub_backend/dormdrop/sublease/publicaccess/PublicSubleaseController.java
+++ unihub-backend/src/main/java/com/unihub/api/unihub_backend/dormdrop/sublease/publicaccess/PublicSubleaseController.java
@@ -1,5 +1,19 @@
 package com.unihub.api.unihub_backend.dormdrop.sublease.publicaccess;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.unihub.api.unihub_backend.dormdrop.sublease.SubleaseService;
+
+@RestController
+@RequestMapping(path = "api/v1/public/subleases")
 public class PublicSubleaseController {
 
+    private final SubleaseService subleaseService;
+
+    public PublicSubleaseController(SubleaseService subleaseService) {
+        this.subleaseService = subleaseService;
+    }
+
+    
 }
