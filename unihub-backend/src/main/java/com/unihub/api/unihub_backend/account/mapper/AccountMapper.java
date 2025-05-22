41a7561771c78f59aab87100380ce9816b7295a2
Ryuichi Yamafuji Lun <92515437+Ryuichi-Yamafuji-Lun@users.noreply.github.com>
2025-05-21 17:02:16 -1000

    incorporated DTO into account

--- unihub-backend/src/main/java/com/unihub/api/unihub_backend/account/mapper/AccountMapper.java
+++ unihub-backend/src/main/java/com/unihub/api/unihub_backend/account/mapper/AccountMapper.java
@@ -1,9 +1,15 @@
 package com.unihub.api.unihub_backend.account.mapper;
 
+import java.time.LocalDateTime;
+import java.util.Set;
+
 import org.springframework.stereotype.Component;
 
 import com.unihub.api.unihub_backend.account.Account;
+import com.unihub.api.unihub_backend.account.dto.AccountRegistrationRequest;
 import com.unihub.api.unihub_backend.account.dto.AccountResponseDTO;
+import com.unihub.api.unihub_backend.accountstatusrole.AccountStatus;
+import com.unihub.api.unihub_backend.accountstatusrole.Role;
 
 @Component
 public class AccountMapper {
@@ -29,5 +35,27 @@ public class AccountMapper {
         }
 
         return dto;
+    }
+
+    public Account fromRegistrationRequest(AccountRegistrationRequest request) {
+        Account account = new Account();
+                account.setEmail(request.getEmail());
+        account.setUsername(request.getUsername());
+        account.setPassword(request.getPassword()); // Encrypt this later
+        account.setFirstName(request.getFirstName());
+        account.setLastName(request.getLastName());
+        account.setDateOfBirth(request.getDateOfBirth());
+        account.setCreatedAt(LocalDateTime.now());
+        account.setUpdatedAt(LocalDateTime.now());
+
+        // Set defaults
+        account.setRoles(Set.of(Role.USER));
+        account.setAccountStatus(AccountStatus.ACTIVE);
+        account.setSumOfRatings(5.0f);
+        account.setNumberOfRatings(1);
+        account.setUnsafeFlag((byte) 0);
+        account.setSuspensionCount((byte) 0);
+
+        return account;
     }
 }
--- unihub-backend/src/main/java/com/unihub/api/unihub_backend/account/publicaccess/PublicAccountService.java
+++ unihub-backend/src/main/java/com/unihub/api/unihub_backend/account/publicaccess/PublicAccountService.java
@@ -1,27 +1,27 @@
 package com.unihub.api.unihub_backend.account.publicaccess;
 
-import java.time.LocalDateTime;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.unihub.api.unihub_backend.account.Account;
 import com.unihub.api.unihub_backend.account.AccountRepository;
+import com.unihub.api.unihub_backend.account.dto.AccountRegistrationRequest;
+import com.unihub.api.unihub_backend.account.mapper.AccountMapper;
 
 @Service
 public class PublicAccountService {
 
     private final AccountRepository accountRepository;
-
-    public PublicAccountService(AccountRepository accountRepository) {
+    private final AccountMapper accountMapper;
+    public PublicAccountService(AccountRepository accountRepository, AccountMapper accountMapper) {
         this.accountRepository = accountRepository;
+        this.accountMapper = accountMapper;
     }
 
     // create account account
     @Transactional
-    public Account createAccount(Account account) {
-        account.setCreatedAt(LocalDateTime.now());
-        account.setUpdatedAt(LocalDateTime.now());
+    public Account registerAccount(AccountRegistrationRequest request) {
+        Account account = accountMapper.fromRegistrationRequest(request);
         return accountRepository.save(account);
     }
 }
