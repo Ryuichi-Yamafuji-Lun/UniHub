48ade86e6d6bc2e489b1cee9c504ca6e82fd91c8
Ryuichi Yamafuji Lun <92515437+Ryuichi-Yamafuji-Lun@users.noreply.github.com>
2025-06-15 22:41:15 -1000

    route to login

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,13 +1,14 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import MainLayout from "@/layout/MainLayout";
 import Landing from "@/pages/Landing";
+import LoginPage from "@/pages/Login";
 
 const AppRoutes = () => {
   return (
     <Routes>
       {/* Landing Page */}
       <Route path="/" element={<MainLayout><Landing /></MainLayout>} />
-
+      <Route path="/login" element={<LoginPage />} />
       {/* Catch-all fallback */}
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
