57181ce9c0bd0a7f16d2116e82b624e2aa9e6ecc
Ryuichi Yamafuji Lun <92515437+Ryuichi-Yamafuji-Lun@users.noreply.github.com>
2025-06-21 21:26:55 -1000

    added sublease data dto to frontend

---
+++ frontend/src/apps/dormdrop/types/SubleaseResponse.ts
@@ -0,0 +1,40 @@
+export const SubleaseAmenity = {
+  WIFI: "WIFI",
+  LAUNDRY: "LAUNDRY",
+  FURNISHED: "FURNISHED",
+  AIR_CONDITIONING: "AIR_CONDITIONING",
+  PET_FRIENDLY: "PET_FRIENDLY",
+} as const;
+
+export type SubleaseAmenity = keyof typeof SubleaseAmenity;
+
+export const Schools = {
+  USC: "USC",
+
+} as const;
+
+export type Schools = keyof typeof Schools;
+
+export interface SubleaseResponse {
+  id: number;
+  datePosted: string;
+  leaseName: string;
+  leasePrice: number;
+  leaseStartDate: string;
+  leaseEndDate: string;
+  leaseImage: string;
+  roomType: string;
+  roomWidth: number;
+  roomDepth: number;
+  leaseDescription: string;
+  leaseAddress: string;
+  longitude: number;
+  latitude: number;
+  amenities: SubleaseAmenity[];
+  school: Schools;
+  ownerUsername: string;
+  ownerProfilePicture: string;
+  sumOfRatings: number;
+  numberOfRatings: number;
+  ownerEmail: string;
+}
