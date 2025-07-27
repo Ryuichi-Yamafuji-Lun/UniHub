58eb8aac51289a78e9772640a07caf2266cbdedd
Ryuichi Yamafuji Lun <92515437+Ryuichi-Yamafuji-Lun@users.noreply.github.com>
2025-07-27 11:10:53 -1000

    made minimal changes to uauthentication navbar

--- frontend/src/components/layout/navbar/MinimalNavbar.tsx
+++ frontend/src/components/layout/navbar/MinimalNavbar.tsx
@@ -3,7 +3,7 @@ import UniHubLogo from "@/assets/UniHubLogo.png";
 
 const MinimalNavBar = () => {
   return (
-    <header className="w-full z-50 sticky top-0 left-0">
+    <header className="w-full z-50 fixed top-0 left-0 shadow-md">
       <div className="px-6 py-3 flex items-center justify-start h-20">
         <Link to="/" className="flex items-center space-x-3 h-full">
           <img src={UniHubLogo} alt="UniHub Logo" className="h-12 w-auto object-contain" />
